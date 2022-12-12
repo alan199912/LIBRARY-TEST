655d19815738162a1b8f40315a3517a15e0a85e6
matilope <mati02171996@gmail.com>
2022-12-12 15:22:16 -0300

    fix: se hacen exports en cada componente por separado

---
+++ projects/ngx-obelisco/option/o-option.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Option, OptionItem } from './models/option.models';
+import { Sizes } from './models/sizes.models';
+
+@Component({
+  selector: 'o-option',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './o-option.component.html',
+  styleUrls: ['./o-option.component.scss']
+})
+export class OOptionComponent implements OnInit {
+  @Input() public option!: Option;
+  @Input() public size: Sizes = 'md';
+
+  @Output() optionSelected = new EventEmitter<string>();
+
+  private firstActive!: OptionItem;
+
+  public ngOnInit(): void {
+    this.firstActive = this.option.items.find((e) => e.active === true)!;
+    this.option.items.map((e) => e !== this.firstActive && (e.active = false));
+  }
+
+  public selectedOption(item: OptionItem): void {
+    if (item !== this.firstActive) {
+      this.firstActive = item;
+      this.optionSelected.emit(item.text);
+    }
+
+    this.option.items.map((e) => e !== item && (e.active = false));
+    item.active = !item.active;
+  }
+}
