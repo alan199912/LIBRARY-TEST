2f784959f79807a2f1b0a48000dc054c6db178bc
Alan199912 <alan199912@gmail.com>
2022-12-22 14:41:47 -0300

    fix: remover ngoninit y constructor vacios

--- src/app/modules/section-components/status-bar/pages/status-bar-overview/status-bar-overview.component.ts
+++ src/app/modules/section-components/status-bar/pages/status-bar-overview/status-bar-overview.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-status-bar-overview',
   templateUrl: './status-bar-overview.component.html',
   styleUrls: ['./status-bar-overview.component.scss']
 })
-export class StatusBarOverviewComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class StatusBarOverviewComponent {}
